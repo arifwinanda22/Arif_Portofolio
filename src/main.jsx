3a47b18737742cae762df1327d481ec1e0a88425
Riflubis4 <arifwinanda3340@gmail.com>
2026-01-30 23:34:28 +0700

    updating deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client' // <-- Ini yang tadi hilang!
 import App from './App.jsx'
-
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Tambahkan basename sesuai nama repo */}
+    {/* Pastikan basename sesuai nama repo GitHub Anda */}
     <BrowserRouter basename="/Arif_Portofolio">
       <App />
     </BrowserRouter>
